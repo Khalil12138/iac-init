4f5e7595c81e11efef0668b483b32f098dd66266
Khalil12138 <150124128+Khalil12138@users.noreply.github.com>
2024-05-05 14:01:17 +0800

    Update ansible_tool.py

--- iac_init/scripts/ansible_tool.py
+++ iac_init/scripts/ansible_tool.py
@@ -3,14 +3,21 @@
 # Copyright: (c) 2022, Wang Xiao <xiawang3@cisco.com>
 
 import os
+import re
 from loguru import logger
 from ansible_runner import run
 from iac_init.conf import settings
 
 def run_ansible_playbook(step: str, option, inventory_path, playbook_path):
-    logger.add(sink=os.path.join(settings.OUTPUT_BASE_DIR, 'iac_init_log', 'iac-init-{}-{}.log'.format(option, step)))
+    logger.add(sink=os.path.join(settings.OUTPUT_BASE_DIR, 'iac_init_log', 'iac-init-{}-{}.log'.format(option, step)),
+               enqueue=True)
 
-    runner = run(playbook=playbook_path, inventory=inventory_path, verbosity=5, stdout_callback="debug")
+    def callback(res):
+        output = re.compile(r'\x1b\[\[?(?:\d{1,2}(?:;\d{0,2})*)?[m|K]').sub('', res['stdout'])
+        logger.info(output)
+
+    runner = run(playbook=playbook_path, inventory=inventory_path, verbosity=5, stdout_callback="debug", quiet=True,
+                 event_handler=callback)
     logger.info(runner.stdout.read())
 
     if runner.status == "successful":
