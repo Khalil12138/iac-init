fdc98c6c8839ba19816fdf443d600778225f4867
Khalil12138 <150124128+Khalil12138@users.noreply.github.com>
2024-05-18 15:17:52 +0800

    Update test_integration.py

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) 2022, Daniel Schmidt <danischm@cisco.com>
+# Copyright: (c) 2022, Wang Xiao <xiawang3@cisco.com>
 
 import os
 import pytest
@@ -12,14 +12,18 @@ import iac_init.cli.main
 pytestmark = pytest.mark.integration
 
 
-def test_iac_init():
+def test_iac_init(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _: '3')
     runner = CliRunner()
     yaml_path = os.path.join("test", "integration", "fixtures", "data")
+
     result = runner.invoke(
         iac_init.cli.main.main,
-        [
-            "-d",
-            yaml_path,
-        ],
+        ["-d", yaml_path],
+        input='yes'
     )
+
+    # Check if the command ran successfully
     assert result.exit_code == 0
+    # Check output message
+    assert result.output.strip() == "IAC initialization completed successfully."  
