f9e02fa288e87704f01757e78af2b78d251ed811
wangxiao <550844811@qq.com>
2024-05-19 14:55:59 +0800

    fix test issue

--- iac_init/conf/__init__.py
+++ iac_init/conf/__init__.py
@@ -3,10 +3,9 @@
 # Copyright: (c) 2022, Wang Xiao <xiawang3@cisco.com>
 
 from iac_init.conf import global_settings
-from iac_init.utils.functional import LazyObject
+from iac_init.utils.functional import LazyObject, empty
 
 ENVIRONMENT_VARIABLE = "IAC_INIT_SETTINGS_MODULE"
-
 
 class SettingsReference(str):
     """
@@ -20,7 +19,6 @@ class SettingsReference(str):
     def __init__(self, value, setting_name):
         self.setting_name = setting_name
 
-
 class LazySettings(LazyObject):
     def _setup(self, name=None):
 
@@ -29,13 +27,13 @@ class LazySettings(LazyObject):
     def __getattr__(self, name):
         """Return the value of a setting and cache it in self.__dict__."""
         _wrapped = None
-        if self._wrapped is not None and self._wrapped == "":
+        if (_wrapped := self._wrapped) is empty:
             self._setup(name)
+            _wrapped = self._wrapped
         val = getattr(self._wrapped, name)
 
         # Special case some settings which require further modification.
-        # This is done here for performance reasons
-        # so the modified value is cached.
+        # This is done here for performance reasons so the modified value is cached.
 
         self.__dict__[name] = val
         return val
@@ -56,14 +54,11 @@ class LazySettings(LazyObject):
         super().__delattr__(name)
         self.__dict__.pop(name, None)
 
-
 class Settings:
     def __init__(self):
-        # update this dict from global settings
-        # (but only for ALL_CAPS settings)
+        # update this dict from global settings (but only for ALL_CAPS settings)
         for setting in dir(global_settings):
             if setting.isupper():
                 setattr(self, setting, getattr(global_settings, setting))
-
 
 settings = LazySettings()
